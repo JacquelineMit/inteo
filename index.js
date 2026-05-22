e9ffa2308296160373878d52ad9f4a05ab2d4ca8
jacqueline-vara02 <jacquelinevv@mail.ru>
2026-05-22 20:13:14 +0300

    кнопка начало

---
+++ index.js
@@ -0,0 +1,5 @@
+let button = document.querySelector(/** css selector -> */ "#Header__Button");
+console.log(button);
+button.addEventListener("click", function (event) {
+  console.log("Открытие меню");
+});
