3cceb2b6af68d8cfbe75a01145cf57d3124d97e7
jacqueline-vara02 <jacquelinevv@mail.ru>
2026-05-29 19:59:26 +0300

    меню

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 let button = document.querySelector(/** css selector -> */ "#Header__Button");
-console.log(button);
+let modalWindow = document.querySelector("#Menu");
+let button__menu = document.querySelector("#Button__menu");
+let menuButtons = modalWindow.querySelectorAll(".mobile-menu__button");
+
+// открытие мобильного меню
 button.addEventListener("click", function (event) {
-  console.log("Открытие меню");
+  open();
 });
+// закрытие мобильного меню
+button__menu.addEventListener("click", function (event) {
+  close();
+});
+// обработка кнопок в мобильном меню
+for (i of menuButtons) {
+  i.addEventListener("click", function (event) {
+    close();
+  });
+}
+
+function open() {
+  modalWindow.setAttribute("aria-hidden", "false");
+  document.body.classList.add("block-scroll");
+}
+
+function close() {
+  modalWindow.setAttribute("aria-hidden", "true");
+  document.body.classList.remove("block-scroll");
+}
